e9c88b92e953ba58406841ecbecd391a148bfdb3
Darran Smith <116950436+DarranS360@users.noreply.github.com>
2025-06-24 16:22:50 +0100

    - removed unused import - added gunicorn to requirements for EC2 persistant hosting

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py - Main Flask Application
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
